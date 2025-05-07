93a5ed0060c48992e493f0d740564f79c1edb908
farrelazhra <farrel.rongso@gmail.com>
2025-05-07 20:39:14 +0700

    second commit

--- app/src/main/java/com/example/pampraktikumfirebase/InsertNoteActivity.java
+++ app/src/main/java/com/example/pampraktikumfirebase/InsertNoteActivity.java
@@ -2,63 +2,124 @@ package com.example.pampraktikumfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class InsertNoteActivity extends AppCompatActivity {
+public class InsertNoteActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView tvEmail;
+    private TextView tvUid;
+    private Button btnKeluar;
     private FirebaseAuth mAuth;
-    private TextView tvEmail, tvUid;
-    private Button btnLogout;
+    private EditText etTitle;
+    private EditText etDesc;
+    private Button btnSubmit;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    Note note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_note);
 
-        // Inisialisasi FirebaseAuth
-        mAuth = FirebaseAuth.getInstance();
-
-        // Inisialisasi view
         tvEmail = findViewById(R.id.tv_email);
         tvUid = findViewById(R.id.tv_uid);
-        btnLogout = findViewById(R.id.btn_keluar);
+        btnKeluar = findViewById(R.id.btn_keluar);
+        mAuth = FirebaseAuth.getInstance();
+        btnKeluar.setOnClickListener(this);
 
-        // Event handler tombol logout
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logOut();
-            }
-        });
+        etTitle = findViewById(R.id.et_title);
+        etDesc = findViewById(R.id.et_description);
+        btnSubmit = findViewById(R.id.btn_submit);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        note = new Note();
+        btnSubmit.setOnClickListener(this);
     }
 
-    // Method logout
-    public void logOut() {
-        mAuth.signOut();
-        Intent intent = new Intent(InsertNoteActivity.this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // makesure user cant go back
-        startActivity(intent);
-    }
-
-    // Override onStart untuk cek user login
     @Override
     protected void onStart() {
         super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             tvEmail.setText(currentUser.getEmail());
             tvUid.setText(currentUser.getUid());
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_keluar) {
+            logOut();
+        } else if (view.getId() == R.id.btn_submit) {
+            submitData();
+        }
+    }
+
+    public void logOut() {
+        mAuth.signOut();
+        Intent intent = new Intent(InsertNoteActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);//makesure user cant go back
+        startActivity(intent);
+    }
+
+    public void submitData() {
+        if (!validateForm()) {
+            return;
+        }
+        String title = etTitle.getText().toString();
+        String desc = etDesc.getText().toString();
+        Note baru = new Note(title, desc);
+        databaseReference.child("notes").child(mAuth.getUid()).push().setValue(baru)
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(InsertNoteActivity.this, "Add data", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(InsertNoteActivity.this, "Failed to Add data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(etTitle.getText().toString())) {
+            etTitle.setError("Required");
+            result = false;
+        } else {
+            etTitle.setError(null);
+        }
+        if (TextUtils.isEmpty(etDesc.getText().toString())) {
+            etDesc.setError("Required");
+            result = false;
+        } else {
+            etDesc.setError(null);
+        }
+        return result;
     }
 }
